619db535dcc0fd46d0d43228ab067965f78e4b62
Jason Voirin <jasonvoirin@gmail.com>
2015-08-02 12:06:29 -0500

    New updates

--- ngFader.js
+++ ngFader.js
@@ -1,77 +1,93 @@
 (function () {
     'use strict';
     angular.module('ngFader', [])
-	.directive('ngFader', function($interval) {
+      .directive('ngFader', function($interval) {
 
-	function link(scope){
+	  function link(scope){
 
-		/* ==================================================================
-			List your images here. 
-			The images array is the only code you should need to change.
-		===================================================================== */
+		//Set your interval time. 4000 = 4 seconds
+		scope.setTime = 4000;
+
+		//List your images here. 
 		scope.images = [{
-			src: 'img/slideshow/slideshow_Image_1_low.jpg',
-			alt: 'Add your image description here'
+			src: 'img/fader-images/1.png',
+			alt: 'The Beach'
 		}, {
-			src: 'img/slideshow/slideshow_Image_2_low.jpg',
-			alt: 'Add your image description here'
+			src: 'img/fader-images/2.png',
+			alt: 'The Beach'
 		}, {
-			src: 'img/slideshow/slideshow_Image_3_low.jpg',
-			alt: 'Add your image description here'
+			src: 'img/fader-images/3.png',
+			alt: 'The Beach'
 		}, {
-			src: 'img/slideshow/slideshow_Image_4_low.jpg',
-			alt: 'Add your image description here'
+			src: 'img/fader-images/4.png',
+			alt: 'The Beach'
+		}, {
+			src: 'img/fader-images/5.png',
+			alt: 'The Beach'
+		}, {
+			src: 'img/fader-images/6.png',
+			alt: 'The Beach'
 		}];
 
+		/*****************************************************
+			STOP! NO FURTHER CODE SHOULD HAVE TO BE EDITED
+		******************************************************/
 
 		//Pagination dots - gets number of images
-	        scope.numberOfImages = scope.images.length;
-	        scope.dots = function(num) {
-	          return new Array(num);   
-	        };
+        scope.numberOfImages = scope.images.length;
+        scope.dots = function(num) {
+          return new Array(num);   
+        };
 
-	        //Pagination - click on dots and change image
-	        scope.selectedImage = 0;
-	        scope.setSelected = function (idx) {
-	          scope.stopSlider();
-	          scope.selectedImage = idx;
-	        };
+        //Pagination - click on dots and change image
+        scope.selectedImage = 0;
+        scope.setSelected = function (idx) {
+          scope.stopSlider();
+          scope.selectedImage = idx;
+        };
 
-	        //Slideshow controls
-	        scope.sliderBack = function() {
-	          scope.stopSlider();
-	          scope.selectedImage === 0 ? scope.selectedImage = scope.numberOfImages - 1 : scope.selectedImage--;
-	        };
+        //Slideshow controls
+        scope.sliderBack = function() {
+          scope.stopSlider();
+          scope.selectedImage === 0 ? scope.selectedImage = scope.numberOfImages - 1 : scope.selectedImage--;
+        };
 
-	        scope.sliderForward = function() {
-	          scope.stopSlider();
-	          scope.autoSlider();
-	        };
+        scope.sliderForward = function() {
+          scope.stopSlider();
+          scope.autoSlider();
+        };
 
-	        scope.autoSlider = function (){
-	          scope.selectedImage < scope.numberOfImages - 1 ? scope.selectedImage++ : scope.selectedImage = 0;
-	        };
+        scope.autoSlider = function (){
+          scope.selectedImage < scope.numberOfImages - 1 ? scope.selectedImage++ : scope.selectedImage = 0;
+        };
 
-	        scope.stopSlider = function() {
-	          $interval.cancel(scope.intervalPromise);
-	          scope.activePause = true;
-	          scope.activeStart = false;
-	        };
+        scope.stopSlider = function() {
+          $interval.cancel(scope.intervalPromise);
+          scope.activePause = true;
+          scope.activeStart = false;
+        };
 
-	        scope.toggleStartStop = function() {
-	          if(scope.activeStart) {
-	          	scope.stopSlider();
-	          } else {
-	          	scope.startSlider();
-	          }
-	        };
-	        
-	        scope.startSlider = function(){
-	          scope.intervalPromise = $interval(scope.autoSlider, 3000);
-	          scope.activeStart = true;
-	          scope.activePause = false;
-	        };
-	        scope.startSlider();
+        scope.toggleStartStop = function() {
+          if(scope.activeStart) {
+          	scope.stopSlider();
+          } else {
+          	scope.startSlider();
+          }
+        };
+        
+        scope.startSlider = function(){
+          scope.intervalPromise = $interval(scope.autoSlider, scope.setTime);
+          scope.activeStart = true;
+          scope.activePause = false;
+        };
+        scope.startSlider();
+
+        scope.show = function(idx){
+        	if (scope.selectedImage==idx) {
+        		return "show";
+        	}
+        };
+        
 
 	}
 
@@ -81,7 +97,7 @@
 	    template: '<div class="ng-fader">'+
 	    		//images will render here
 			'<ul>' + 
-				'<li ng-repeat="image in images" ng-click="toggleStartStop()" ng-swipe-right="sliderBack()" ng-swipe-left="sliderForward()"><img data-ng-src="{{image.src}}" data-ng-alt="{{image.alt}}" ng-show="selectedImage==$index"/></li>' + 
+				'<li ng-repeat="image in images" ng-click="toggleStartStop()" ng-swipe-right="sliderBack()" ng-swipe-left="sliderForward()"><img data-ng-src="{{image.src}}" data-ng-alt="{{image.alt}}" ng-class="show($index)"/></li>' + 
 			'</ul>' + 
 			//pagination dots will render here
 			'<div class="ng-fader-pagination">' + 
@@ -109,6 +125,6 @@
 		'</div>',
 		link: link
 	  };
-	});
+      });
 
 }());
